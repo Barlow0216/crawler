0bca2a778183953614cb502a1ecfcd5eab3e34b7
williambarlow <williambarlow0216@gmail.com>
2024-10-14 06:27:55 -0700

    initial

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git filter-branch --env-filter '
+
+CORRECT_NAME="William Barlow"
+CORRECT_EMAIL="barlowilljos6@outlook.com"
+
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+
+' --tag-name-filter cat -- --branches --tags
